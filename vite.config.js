b877965f77dcb0c3aadfa887a595b8a66e5db455
Valmocina Sean <sean.matthew.valmocina@adamson.edu.ph>
2025-12-02 23:59:48 +0800

    Fix blank screen issue

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: Replace 'YOUR_REPO_NAME' with the exact name of your GitHub repository
-  // Example: base: '/billiards-club/',
-  base: '/YOUR_REPO_NAME/',
+  base: '/', // <--- ADD THIS LINE
 })
