ff001af61d76aaad500fea04c56a86d68c996923
Valmocina Sean <sean.matthew.valmocina@adamson.edu.ph>
2025-12-09 00:36:06 +0800

    Fix vite config for Vercel

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // <--- ADD THIS LINE
+  // Remove the 'base' property for Vercel deployments
+  // base: '/billiards-club/', 
 })
